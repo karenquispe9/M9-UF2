de5f0b44de2aed0bbaa5febffb56e44b47522f81
karen <karenquispego97@gmail.com>
2025-01-16 23:44:30 +0100

    estructura de clase motor

--- sleepicoet/src/main/java/Motor.java
+++ sleepicoet/src/main/java/Motor.java
@@ -1,3 +1,20 @@
-public class Motor {
+import java.util.Random;
 
+public class Motor extends Thread {
+    private int potenciaActual; // Potencia actual del motor
+    private int potenciaObjectiu; // Potencia objetivo
+    private final Random random = new Random(); 
+
+    public Motor(String name) {
+       
+    }
+
+    public synchronized void setPotencia(int potenciaObjectiu) {
+       
+    }
+
+    @Override
+    public void run() {
+        
+    }
 }
