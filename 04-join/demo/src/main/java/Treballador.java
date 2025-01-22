5ddf3c3290a21524d68187870d7a350b8ed04e00
karen <karenquispego97@gmail.com>
2025-01-22 21:55:30 +0100

    estructura clase trabajador, constructor, metodos y getters

--- 04-join/demo/src/main/java/Treballador.java
+++ 04-join/demo/src/main/java/Treballador.java
@@ -1,3 +1,41 @@
-public class Treballador {
+import java.util.Random;
 
+public class Treballador extends Thread {
+    private final float nouAnualBrut;
+    private final int edatIniciTreball;
+    private final int edatFiTreball;
+    private int edatActual;
+    private float cobrat;
+    private final Random rnd;
+
+
+    public Treballador(String nom, float nouAnualBrut, int edatIniciTreball, int edatFiTreball) {
+        super(nom);
+        this.nouAnualBrut = nouAnualBrut;
+        this.edatIniciTreball = edatIniciTreball;
+        this.edatFiTreball = edatFiTreball;
+        this.edatActual = 0;
+        this.cobrat = 0.0f;
+        this.rnd = new Random();
+    }
+
+    public void cobra() {
+    }
+
+    public void pagaImpostos() {
+    }
+
+    
+    @Override
+    public void run() {
+       
+    }
+
+    public float getCobrat() {
+       
+    }
+
+  
+    public int getEdat() {
+    }
 }
